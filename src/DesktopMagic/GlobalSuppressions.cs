3c732117d3fb48a31459cbbf2b4d3646b1feb59e
Stone_Red <56473591+Stone-Red-Code@users.noreply.github.com>
2024-02-22 09:50:30 +0100

    Remove auto update

--- src/DesktopMagic/GlobalSuppressions.cs
+++ src/DesktopMagic/GlobalSuppressions.cs
@@ -8,3 +8,4 @@ using System.Diagnostics.CodeAnalysis;
 [assembly: SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "Windows only application")]
 [assembly: SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "No need to")]
 [assembly: SuppressMessage("Minor Code Smell", "S1075:URIs should not be hardcoded", Justification = "<Pending>")]
+[assembly: SuppressMessage("Critical Code Smell", "S2696:Instance members should not write to \"static\" fields", Justification = "<Pending>", Scope = "member", Target = "~P:DesktopMagic.DataContexts.MainWindowDataContext.Settings")]
