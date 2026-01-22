3dce9ab59a4feee9d56ff4e19820830d0459d449
Stone_Red <56473591+Stone-Red-Code@users.noreply.github.com>
2026-01-22 14:25:05 +0100

    Add CreatePluginDialog for plugin creation and update search boxes with icons

---
+++ src/DesktopMagic/Dialogs/CreatePluginDialog.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+
+namespace DesktopMagic.Dialogs;
+
+public partial class CreatePluginDialog : Wpf.Ui.Controls.FluentWindow
+{
+    public string ResponseText
+    {
+        get => textBox.Text;
+        set => textBox.Text = value;
+    }
+
+    public CreatePluginDialog()
+    {
+        InitializeComponent();
+
+        Resources.MergedDictionaries.Add(App.LanguageDictionary);
+    }
+
+    private void OkButton_Click(object sender, RoutedEventArgs e)
+    {
+        DialogResult = true;
+    }
+
+    private void CancelButton_Click(object sender, RoutedEventArgs e)
+    {
+        DialogResult = false;
+    }
+}
